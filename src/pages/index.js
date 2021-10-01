d112da6aa5280672685eafe60ce3df5379415056
Arisa Fukuzaki <25793187+schabibi1@users.noreply.github.com>
2021-10-01 15:37:27 +0200

    delete unecessary comment-outs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,33 +2,26 @@ import * as React from "react"
 
 import { graphql } from 'gatsby'
 import { sbEditable } from '@storyblok/storyblok-editable'
-// 2. dynamicComponents
 import DynamicComponent from "../components/dynamicComponent"
-// 3. 🪝
 import useStoryblok from "../lib/storyblok"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-// 3. 🪝 location
 const IndexPage = ({ data, location }) => {
   let story = data.storyblokEntry
   // story.content = JSON.parse(story.content)
-  // 3. 🪝
   story = useStoryblok(story, location)
 
-  // 2. dynamicComponents
   const components = story.content.body.map(blok => {
     return (<DynamicComponent blok={blok} key={blok._uid} />)
   })
 
   return (
     <Layout>
-      {/* // 3. 🪝 */}
       <div {...sbEditable(story.content)}>
         <Seo title="Home" />
         <h1>{ story.name }</h1>
-        {/* 2. dynamicComponents */}
         { components }
       </div>
     </Layout>
@@ -37,7 +30,6 @@ const IndexPage = ({ data, location }) => {
 
 export default IndexPage
 
-// 1. add page name
 export const query = graphql`
   query HomeQuery {
     storyblokEntry(full_slug: {eq: "home"}) {
